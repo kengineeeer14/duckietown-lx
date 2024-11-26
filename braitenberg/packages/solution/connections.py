b08c690e7f1e15230aecc021debffdd531ea5b0b
ken <kengineeeer14@gmail.com>
2024-11-27 01:52:07 +0900

    change motor parameter, const and gain

--- braitenberg/packages/solution/connections.py
+++ braitenberg/packages/solution/connections.py
@@ -7,8 +7,10 @@ def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # TODO: write your function instead of this one
     res = np.zeros(shape=shape, dtype="float32")
     # these are random values
-    res[100:150, 100:150] = 1
-    res[300:, 200:] = 1
+    res[:, :320] = 0
+    res[300:, 100:320] = 0.1
+    res[:, 320:] = 0
+    res[300:, 320:540] = 0
     # ---
     return res
 
@@ -17,6 +19,9 @@ def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # TODO: write your function instead of this one
     res = np.zeros(shape=shape, dtype="float32")
     # these are random values
-    res[100:150, 100:300] = -1
+    res[:, 320:] = 0
+    res[300:, 320:540] = 0.1
+    res[:, :320] = 0
+    res[300:, 100:320] = 0
     # ---
     return res
